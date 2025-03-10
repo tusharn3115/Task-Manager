6fd04ce046779e409ea1a6bc70aef760bb8e1f13
tusharn3115 <negitushar81@gmail.com>
2025-03-10 13:10:41 +0530

    Update TeamSwitcher component with Popover and Button; add Button component; modify global styles

--- components/navbar/team-switcher.tsx
+++ components/navbar/team-switcher.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Popover, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Avatar, AvatarImage } from '../ui/avatar'
 
 const TeamSwitcher = () => {
   return (
-    <div>TeamSwitcher</div>
+    <Popover>
+      <PopoverTrigger>
+        <Button variant="outline" role='combobox' aria-label='Select a team' className='w-[200px] justify-between'>
+          <Avatar className='mr-2 h-6 w-6'>
+            <AvatarImage src="http://avatar.vercel.sh/Tushar.png" alt="Profile Image" />
+          </Avatar>
+        </Button>
+      </PopoverTrigger>
+    </Popover>
   )
 }
 
